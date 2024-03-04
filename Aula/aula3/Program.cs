695eb89050eb6842c364955009a2031c9af83bf7
Gabriel de Oliveira <419150@unoesc.lan>
2024-03-04 20:22:13 -0300

    segundoCommit

---
+++ Aula/aula3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Substituindo conteúdo de strings
+
+string nomeCompleto = "Gabriel de Olivera";
+nomeCompleto = 
+    nomeCompleto.Replace("de", "Dell");
+Console.WriteLine(nomeCompleto);
+
+//Comparação de Strings
+bool isNomeEqual =
+    (nomeCompleto == "Gabriel de Oliveira");
+bool isNomeEqual2 =
+    string.Equals(nomeCompleto, "Gabriel dell Oliveira");
+
+Console.WriteLine($"Primeiro resultado: {isNomeEqual}");
+Console.WriteLine($"Segundo resultado: {isNomeEqual2}");
+
