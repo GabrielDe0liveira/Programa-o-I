7cae99fa1dafdfd71b7e343ecb0fe8b7f9b58938
Gabriel de Oliveira <gabriel.o99@outlook.com>
2024-03-04 21:09:34 -0300

    terceiroCommit

--- Aula/aula3/Program.cs
+++ Aula/aula3/Program.cs
@@ -1,5 +1,4 @@
 ﻿//Substituindo conteúdo de strings
-
 string nomeCompleto = "Gabriel de Olivera";
 nomeCompleto = 
     nomeCompleto.Replace("de", "Dell");
@@ -14,3 +13,14 @@ bool isNomeEqual2 =
 Console.WriteLine($"Primeiro resultado: {isNomeEqual}");
 Console.WriteLine($"Segundo resultado: {isNomeEqual2}");
 
+//Tipos Numéricos
+ /* 
+    sbyte: Armazena valores entre -128 e 127.
+    short: Armazena valores entre -32,768 e 32,767.
+    int: Armazena valores entre -2,147,483,648 e 2,147,483,647.
+    long: Armazena valores entre -9,223,372,036,854,775,808 e -9,223,372,036,854,775,807
+ */
+ float myFloat = 10f;
+ double myDouble = 5d;
+ decimal myDecimal = 7m;
+ int myInteger = 0;
